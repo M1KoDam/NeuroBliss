67eb817b2a4bf79f71c0bf0e1361eead5f7115c9
Mike <kononov.mixa2045@gmail.com>
2024-01-07 16:18:15 +0500

    add model requests generator

--- Server/Application/Models/model_requests.py
+++ Server/Application/Models/model_requests.py
@@ -1,13 +1,40 @@
-instruments = ['guitar', 'piano', 'violin', 'drums', 'saxophone', 'flute', 'trumpet', 'bass guitar',
-               'clarinet', 'cello', 'keyboard', 'synthesizer', 'ukulele', 'harmonica', 'electric guitar',
-               'tambourine', 'accordion', 'maracas', 'xylophone', 'banjo', 'organ']
+from random import sample, choice
 
-style = ['pop', 'rock', 'hip-hop', 'electronic', 'jazz', 'classical', 'reggae', 'country',
-         'rhythm and blues', 'metal', 'punk', 'indie', 'folk', 'latin', 'gospel', 'blues',
-         'club/dance', 'club', 'dubstep', 'reggaeton', 'soul', 'techno', 'disco',
-         'classic rock', 'alternative']
 
-mood = [['epic', 'happy', 'sad', 'energetic', 'relaxed', 'uplifting', 'melancholic', 'groovy',
-         'thoughtful', 'emotional', 'empowering', 'dreamy', 'aggressive', 'chill', 'romantic',
-         'mysterious', 'funky', 'passionate', 'calm', 'inspirational', 'intense', 'playful',
-         'nostalgic', 'rebellious', 'peaceful']]
+MOODS = {
+    "happy": ["energetic", "uplifting", "groovy", "playful", "inspirational", "groovy"],
+    "sad": ["melancholic", "emotional", "nostalgic", "dark", "soulful"],
+    "calm": ["relaxed", "peaceful", "soulful", "thoughtful", "dreamy", "mysterious", "chill"],
+    "aggressive": ["intense", "dark", "rebellious", "epic", "energetic"],
+    "romantic": ["passionate", "mysterious"],
+    "motivating": ["empowering", "inspirational", "epic", "uplifting"]
+}
+
+STYLES = {
+    "happy": ["pop", "reggae", "disco", "funk", "club", "dubstep", "techno"],
+    "sad": ["jazz", "blues", "sadcore", "dark ambient", "darkwave"],
+    "calm": ["jazz", "ambient", "classical", "meditative"],
+    "aggressive": ["rock", "metal", "punk", "hardcore", "heavy-metal", "dubstep"],
+    "romantic": ["R&B", "soul", "jazz", "pop", "classical"],
+    "motivating": ["hip-hop", "electronic", "pop", "rock", "dubstep"]
+}
+
+INSTRUMENTS = {
+    "happy": ["guitar", "drums", "synth", "bass guitar"],
+    "sad": ["violin", "piano", "saxophone", "acoustic guitar", "flute"],
+    "calm": ["piano", "acoustic Guitar", "flute", "harp", "cello", "violin"],
+    "aggressive": ["electric guitar", "bass guitar", "heavy drums", "synth"],
+    "romantic": ["violin", "piano", "flute", "clarinet"],
+    "motivating": ["electronic Drums", "electric Guitar", "synth", "trumpet", "drums"]
+}
+
+
+def get_model_request_by_mood(mood: str) -> str:
+    moods = [mood, *sample(MOODS[mood], 2)]
+    style = choice(STYLES[mood])
+    instruments = sample(INSTRUMENTS[mood], 3)
+    return f"{', '.join(moods)} {style} track with {', '.join(instruments[:-1])} and {instruments[-1]}"
+
+
+if __name__ == "__main__":
+    print(get_model_request_by_mood("romantic"))
