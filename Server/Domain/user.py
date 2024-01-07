2a7dd22123f41ca5e830fe3669cbee1eb5601625
Mike <kononov.mixa2045@gmail.com>
2024-01-07 17:08:52 +0500

    fix multiple user creation

--- Server/Domain/user.py
+++ Server/Domain/user.py
@@ -1,5 +1,7 @@
 
 class User:
+    __created_users = {}
+
     def __init__(self, user_id: str, liked: list = None, playlists: list = None):
         self.user_id: str = user_id
 
@@ -13,3 +15,14 @@ class User:
 
     def add_music_to_liked(self, music_id: str):
         self.liked.append(music_id)
+
+    def __new__(cls, *args, **kwargs):
+        if args[0] in cls.__created_users.keys():
+            return cls.__created_users[args[0]]
+        new_user = super().__new__(cls)
+        cls.__created_users[args[0]] = new_user
+        return new_user
+
+
+if __name__ == '__main__':
+    user = User("jewfwef0")
