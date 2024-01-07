928a4a7c583e3264806d69391881ca66444fb037
MaxD3mage <terzi.kirill@gmail.com>
2024-01-07 16:23:01 +0500

    added to liked client, and some remake

--- Client/Application/client_api.py
+++ Client/Application/client_api.py
@@ -2,6 +2,8 @@ from pydantic import BaseModel
 import httpx
 from hashlib import sha256
 
+server = "127.0.0.1:8000"
+
 
 class UserInformation(BaseModel):
     login: str
@@ -23,7 +25,7 @@ class MusicInfo(BaseModel):
 def register_user(login: str, password: str):
     user_info = UserInformation(login=login, password=sha256(password.encode()).hexdigest(), user_id=None)
     try:
-        response = httpx.post("http://127.0.0.1:8000/auth/sign-up", json=user_info.dict())
+        response = httpx.post(f"http://{server}/auth/sign-up", json=user_info.dict())
         return response.json()
     except httpx.ConnectError:
         return httpx.ConnectError
@@ -32,7 +34,7 @@ def register_user(login: str, password: str):
 def login_user(login: str, password: str):
     user_info = UserInformation(login=login, password=sha256(password.encode()).hexdigest(), user_id=None)
     try:
-        response = httpx.post("http://127.0.0.1:8000/auth/sign-in", json=user_info.dict())
+        response = httpx.post(f"http://{server}/auth/sign-in", json=user_info.dict())
         return response.json()
     except httpx.ConnectError:
         return httpx.ConnectError
@@ -40,27 +42,33 @@ def login_user(login: str, password: str):
 
 def get_music_generation(user_id: str, style_music: list):
     user_get_music = UserGetMusic(user_id=user_id, style_music=style_music)
-    try:
-        response = httpx.post("http://127.0.0.1:8000/music/get_music", json=user_get_music.dict(), timeout=None)
-        path = response.headers["user_music_path"] + response.headers["id"] + ".wav"
-        with open(path, "wb") as file:
-            for chunk in response.iter_bytes():
-                file.write(chunk)
-
-        return {"path": path}
-    except httpx.ConnectError:
-        return {"status": "Bad connection", "path": None}
+    return _connection_to_server_get_music(user_get_music, f"http://{server}/music/get_music")
 
 
 def get_music_by_id(music_id: str, user_id: str):
     music_info = MusicInfo(music_id=music_id, user_id=user_id)
+    return _connection_to_server_get_music(music_info, f"http://{server}/music/get_music_by_id")
+
+
+def download_music_by_id(music_id: str, user_id: str):
+    music_info = MusicInfo(music_id=music_id, user_id=user_id)
+    return _connection_to_server_get_music(music_info, f"http://{server}/music/download_music_by_id")
+
+
+def _connection_to_server_get_music(music_info, server_str: str):
     try:
-        response = httpx.post("http://127.0.0.1:8000/music/get_music_by_id", json=music_info.dict(), timeout=None)
+        response = httpx.post(server_str, json=music_info.dict(), timeout=None)
         path = response.headers["user_music_path"] + response.headers["id"] + ".wav"
         with open(path, "wb") as file:
             for chunk in response.iter_bytes():
                 file.write(chunk)
 
-        return {"status": "OK", "path": path}
+        return {"status": True, "path": path}
     except httpx.ConnectError:
         return {"status": "Bad connection", "path": None}
+
+
+def add_to_liked(music_id: str, user_id: str):
+    json = {"music_id": music_id, "user_id": user_id}
+    response = httpx.post(f"http://{server}/music/add_music_to_playlist", json=json, timeout=None)
+    return response.json()
