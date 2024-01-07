63e6ee93f3895c35d5c3c13b792555c75d6567a0
Mike <kononov.mixa2045@gmail.com>
2024-01-07 17:56:49 +0500

    fix music_item multiple creation

--- Server/Domain/music_item.py
+++ Server/Domain/music_item.py
@@ -1,4 +1,6 @@
 class MusicItem:
+    __created_music = {}
+
     def __init__(self, music_id: str, path: str, phrase: str, length_in_seconds: int, is_ready=False):
         self.id: str = music_id
         self.path: str = path
@@ -12,3 +14,10 @@ class MusicItem:
 
     def copy(self):
         return MusicItem(self.id, self.path, self.phrase, self.length_in_seconds, self.is_ready)
+
+    def __new__(cls, *args, **kwargs):
+        if args[0] in cls.__created_music.keys():
+            return cls.__created_music[args[0]]
+        new_user = super().__new__(cls)
+        cls.__created_music[args[0]] = new_user
+        return new_user
